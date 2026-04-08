b9a47b0890720c0f3de4f4fbc99b0dc09ea0c8d8
jwaher-x <jawaher.5alidfnn@gmail.com>
2026-04-08 13:19:55 +0300

    todo 1

--- 6-4-express-request-data/server.js
+++ 6-4-express-request-data/server.js
@@ -105,9 +105,10 @@ LAB SETUP INSTRUCTIONS
  */
 
 //import express
-
+import express from "express";
 
 // create express app instance to create web server
+const app = express();
 
 
 
@@ -124,5 +125,7 @@ LAB SETUP INSTRUCTIONS
 
 
 // Start the server by listening
-
+app.listen(3000, () => {
+  console.log("API running at http://localhost:3000");
+});
 
